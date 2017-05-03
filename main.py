b556a7c4575b65206940e22f900fc903f565c43e
liuzhuocpp <liuzhuocpp@qq.com>
2017-05-03 09:26:09 +0800

    继续整理

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import word2vec
 import numpy    
 import codecs
+from sklearn.cluster import KMeans
 
 
 
@@ -9,7 +10,6 @@ import codecs
 # word2vec.word2clusters('data/5.txt', 'out/5-clusters.txt', 100, verbose=True)
 
 # word2vec.word2vec('./data/5.txt', './out/5.bin', size=100, verbose=True)
-model = word2vec.load('out/5.bin')
 
 # print model.vocab
 # print model.vectors.shape
@@ -29,77 +29,70 @@ model = word2vec.load('out/5.bin')
 
 # print cosine_similarity(model, u'服务', u"增值")
 
-from sklearn.cluster import KMeans
 
-data5_list = []
-for wordVec in model.vectors:
-    wordVecList = []
-    for x in wordVec:
-        wordVecList.append(x)
-    data5_list.append(wordVecList)
+def solve(dataId):
+    model = word2vec.load('out/5.bin')
 
-
-
-ClustersNumber = 10
-WordNumber = len(data5_list)
-
-allCluster = []
-for i in xrange(ClustersNumber):
-    allCluster.append([])
-
-
-kmeans = KMeans(n_clusters=ClustersNumber, random_state=0).fit(data5_list)
-
-
-print model.vocab[0]
-print model.vocab[1]
-label = kmeans.labels_
-scores = []
-for i in xrange(WordNumber):
-    scores.append(kmeans.score([model.vectors[i]]) )
+    data5_list = []
+    for wordVec in model.vectors:
+        wordVecList = []
+        for x in wordVec:
+            wordVecList.append(x)
+        data5_list.append(wordVecList)
 
 
 
-print label
+    ClustersNumber = 10
+    WordNumber = len(data5_list)
 
-for i in xrange(len(label)):
-    allCluster[label[i]].append(i)
-    # print "I: " , i, label[i]
-
-output = codecs.open("out/ans5.txt", "w", "utf-8")
-
-
-def comparator(a, b):
-
-    vala = scores[a]
-    valb = scores[b]
-
-    if vala > valb: return 1
-    elif vala == valb : return 0
-    else : return -1
-
-for clusterId in xrange(len(allCluster)):
-    output.write("-----------------------------------cluster " + str(clusterId) + ":\n")
-    # print "cluster ", clusterId, ":"
+    allCluster = []
+    for i in xrange(ClustersNumber):
+        allCluster.append([])
 
 
-    # print allCluster[clusterId][:30]
-
-    # for x in allCluster[clusterId][:30]:
-    #     print x, model.vocab[x], scores[x]
-
-    allCluster[clusterId].sort(cmp = comparator, reverse = True)
+    kmeans = KMeans(n_clusters=ClustersNumber, random_state=0).fit(data5_list)
 
 
-    
-    for x in allCluster[clusterId][:30]:
-        output.write(model.vocab[x] + "  " + str(scores[x])  + "\n")
+    print model.vocab[0]
+    print model.vocab[1]
+    label = kmeans.labels_
+    scores = []
+    for i in xrange(WordNumber):
+        scores.append(kmeans.score([model.vectors[i]]) )
+
+
+
+    # print label
+
+    for i in xrange(len(label)):
+        allCluster[label[i]].append(i)
+
+
+    output = codecs.open("out/ans5.txt", "w", "utf-8")
+
+
+    def comparator(a, b):
+
+        vala = scores[a]
+        valb = scores[b]
+
+        if vala > valb: return 1
+        elif vala == valb : return 0
+        else : return -1
+
+    for clusterId in xrange(len(allCluster)):
+        output.write("-----------------------------------cluster " + str(clusterId) + ":\n")
         
+        
+        allCluster[clusterId].sort(cmp = comparator, reverse = True)
+
 
         
+        for x in allCluster[clusterId][:30]:
+            output.write(model.vocab[x] + "  " + str(scores[x])  + "\n")
 
 
- 
+solve(5)
 
 
 
